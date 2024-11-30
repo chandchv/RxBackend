f2a18571e0bbd0f95fb0aaccf0978c6a0fafabc4
chandchv <chandchv@gmail.com>
2024-11-30 08:47:49 +0530

    sh: line 1: q: command not found

--- users/models.py
+++ users/models.py
@@ -9,9 +9,11 @@ class UserProfile(models.Model):
     medical_degree = models.CharField(max_length=100, null=True, blank=True)
     license_number = models.CharField(max_length=50, null=True, blank=True)
     state_council = models.CharField(max_length=100, null=True, blank=True)
+    clinic_name = models.CharField(max_length=255, blank=True, null=True)
     phone_number = models.CharField(max_length=15, null=True, blank=True)
     address = models.TextField(null=True, blank=True)
     pincode = models.CharField(max_length=10, null=True, blank=True)
+    year_of_registration = models.CharField(max_length=4, null=True, blank=True)
 
     def __str__(self):
         return f"{self.user.username}'s profile"
